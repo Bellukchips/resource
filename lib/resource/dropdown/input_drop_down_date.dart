2c9c88910df02c009414d8bba462f890f3807173
bellukchips <bellukchips@gmail.com>
2022-09-14 20:13:28 +0700

    input dropdown date

--- lib/resource/dropdown/input_drop_down_date.dart
+++ lib/resource/dropdown/input_drop_down_date.dart
@@ -7,45 +7,31 @@ class InputDropDownDate extends StatefulWidget {
   final String? labelText;
   final TextStyle? valueStyle;
   final Color? colorIcon;
-
-  const InputDropDownDate({Key? key, required this.labelText,required this.valueStyle, this.colorIcon}) : super(key: key);
+  final VoidCallback onPressed;
+  final DateTime? selectedDate;
+  const InputDropDownDate(
+      {Key? key,
+      required this.labelText,
+      required this.valueStyle,
+      this.colorIcon,
+      required this.onPressed,
+      this.selectedDate})
+      : super(key: key);
 
   @override
   _InputDropDownDateState createState() => _InputDropDownDateState();
 }
 
 class _InputDropDownDateState extends State<InputDropDownDate> {
-  DateTime? selectedDate = DateTime.now();
-  ValueChanged<DateTime?>? selectDate;
-
-  ///
-  ///
-  /// function to select date
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: selectedDate!,
-      firstDate: DateTime(2015),
-      initialDatePickerMode: DatePickerMode.day,
-      lastDate: DateTime(2101),
-    );
-    if (picked != null && picked != selectedDate) {
-      setState(() {
-        selectedDate = picked;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return InputDropdown(
       colorIcon: widget.colorIcon,
       labelText: widget.labelText,
-      valueText: DateFormat.yMMMd().format(selectedDate!),
+      valueText: DateFormat.yMMMd().format(widget.selectedDate!),
       valueStyle: widget.valueStyle,
       onPressed: () {
-        _selectDate(context);
+        widget.onPressed();
       },
     );
   }
