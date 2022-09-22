ffdaac5d6ddc82bd9f201fa810ef2c2741b628c3
bellukchips <bellukchips@gmail.com>
2022-09-22 10:08:29 +0700

    circle image

--- lib/resource/images/circle_image.dart
+++ lib/resource/images/circle_image.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class CircleImage extends StatelessWidget {
-  const CircleImage({ Key? key, this.height = 60, this.width = 60, this.image }) : super(key: key);
+  const CircleImage(
+      {Key? key, this.height = 60, this.width = 60, this.image, this.fit})
+      : super(key: key);
   final double? height;
   final double? width;
   final String? image;
+  final BoxFit? fit;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +15,8 @@ class CircleImage extends StatelessWidget {
       height: height,
       width: width,
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        image: DecorationImage(
-          image: NetworkImage(image!),fit: BoxFit.cover
-        )
-      ),
-      
+          shape: BoxShape.circle,
+          image: DecorationImage(image: NetworkImage(image!), fit: fit)),
     );
   }
 }
