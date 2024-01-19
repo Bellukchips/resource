292d43fbcac6053b289dbb1550cd83b2b84861d9
Bellukchips <bellukchips@gmail.com>
2024-01-19 17:12:52 +0700

    commit

---
+++ lib/resource/bottom_navbar/bottom_navbar.dart
@@ -0,0 +1,1 @@
+export 'animated_bottom_navbar.dart';
--- lib/resource/resource.dart
+++ lib/resource/resource.dart
@@ -1,10 +1,11 @@
 export 'appbars/app_bars.dart';
+export 'bottom_navbar/bottom_navbar.dart';
 export 'buttons/buttons.dart';
+export 'clipper/clipper.dart';
 export 'dividers/dividers.dart';
+export 'dots/dots.dart';
+export 'dropdown/dropdown.dart';
 export 'images/images.dart';
 export 'inputText/input_texts.dart';
 export 'loading/loadings.dart';
 export 'texts/texts.dart';
-export 'dots/dots.dart';
-export 'clipper/clipper.dart';
-export 'dropdown/dropdown.dart';
